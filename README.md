060a52fa1bf52aeda5b132219cc31da488596444
Maruton Maru <marutongit@gmail.com>
2015-08-03 02:39:58 +0900

    First commit.

---
+++ README.md
@@ -0,0 +1,22 @@
+# Sample_ShadowProjector
+
+## What is this?
+
+Shadow projector sample for unity5.    
+
+## Feature
+
+Shadow to small/disappear when moving to fry high.    
+Shadow projector using to upright direction.
+
+## How to use
+
+See blog: ' Comming soon '    
+http://dennou-note.blogspot.jp/  
+
+
+## License
+
+English, Please see 'LICENSE' file.  
+Japanese, Please see 'LICENSE_JP' file.  
+
---
+++ Sample_ShadowProjector/Assets/cs_ShadowProjector.cs
@@ -0,0 +1,27 @@
+﻿/*
+ *	@file	cs_ShadowProjector
+ *	@note		None 
+ *	@attention	
+ *				[cs_ShadowProjector.cs]
+ *				Copyright (c) [2015] [Maruton]
+ *				This software is released under the MIT License.
+ *				http://opensource.org/licenses/mit-license.php
+ */
+using UnityEngine;
+using System.Collections;
+
+public class cs_ShadowProjector : MonoBehaviour {
+	Quaternion ShadowRot = Quaternion.Euler(270, 90, 0);	//!< Upright Degree of Shadow Projector(this). 
+	Vector3 Distance;										//!< Distance of Shadow Projector(this). 
+	Transform FakeCastObject;								//!< Fake cast shadow object. 
+
+	void Start () {
+		FakeCastObject = transform.parent.transform;	//Get Transform of Parent object.
+		Distance = transform.localPosition;				//Get distance of parent to Shadow Projector(this).
+	}
+	
+	void Update () {
+		transform.rotation = ShadowRot;							//Keep upright degree.
+		transform.position = FakeCastObject.position + Distance; //Keep distance from parent.(Important: Must be parent's scale <1,1,1>). 
+	}
+}
